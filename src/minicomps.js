265e024ca03d6e149ee49800a9c98ad423b8e9c5
Keith Peters <kp@bit-101.com>
2021-06-26 19:55:06 -0400

    bump version number

--- src/minicomps.js
+++ src/minicomps.js
@@ -25,7 +25,7 @@ import { VBox } from "./vbox.js";
 import { VSlider } from "./vslider.js";
 import { Window } from "./window.js";
 
-const version = "0.1.0";
+const version = "0.1.1";
 
 export {
   version,
